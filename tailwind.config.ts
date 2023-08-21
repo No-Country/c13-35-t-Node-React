5b79fd83bf628cee93702d2c3da1400c987658b6
iairkap <iairkap@gmail.com>
2023-08-21 18:50:51 -0300

    updates-tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "color-bg": "#09595A",
+        "color-button": "#576702",
+        "color-button-hover": "#3F4A05",
+        "color-text-black": "#191414",
+        "color-text-white": "#FFFFFF",
+        "color-form-bg": "rgba(87, 103, 2, 0.35)",
       },
     },
   },
+
   plugins: [],
-}
-export default config
+};
+export default config;
