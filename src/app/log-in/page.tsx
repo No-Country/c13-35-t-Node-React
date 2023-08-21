f2f7d709e5ea50acdb2feaa24bf2624e6384ca92
iairkap <iairkap@gmail.com>
2023-08-21 18:57:21 -0300

    updates/picture

--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 export default function Landing(): JSX.Element {
     return (
         <div>
-            <Image src="/images/landing.png" alt="landing" width={500} height={500} />
         </div>
     )
 }
