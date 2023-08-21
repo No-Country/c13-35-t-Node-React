fafcbcf910e77112239c9a5bd894df82a4f48937
iairkap <iairkap@gmail.com>
2023-08-21 17:33:54 -0300

    fix-typescript-errors

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// Definimos una interfaz para las props del componente
+interface PageProps {
+    // Aquí puedes agregar las propiedades que esperas recibir. Por ejemplo:
+    // title?: string;  // Una propiedad opcional de título
+    // content: string;  // Una propiedad requerida de contenido
+
+    title?: string;
+    content: string;
+}
+
+const Page: React.FC<PageProps> = ({ title, content }) => {
+    return (
+        <div>
+            <h1>{title} hola soy el landing</h1>
+            <p>{content}</p>
+        </div>
+    );
+}
+export default Page;
