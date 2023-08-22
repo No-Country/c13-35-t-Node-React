8fd24255ef3adab769056b5c5fad5eb0f73f3259
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-08-22 17:51:39 -0500

    [FEATURE] added customization and banner

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -3,8 +3,30 @@ import NavBar from "../components/navBar/navBar";
 
 export default function Landing(): JSX.Element {
     return (
-        <div>
-            <h1>Landing</h1>
+        <div className='w-full h-full m-auto text-color-text-black font-inriasans'>
+            <section className='bg-banner py-[150px] bg-no-repeat bg-cover bg-center'>
+                <div className='max-w-[1020px] m-auto' >
+                    <h1 className='text-5xl text-center'>Lorem ipsum dolor, sit amet consectetur.</h1>
+                    <p className='text-xl m-auto text-center max-w-2xl justify-center my-20'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur delenit.</p>
+                    <div className='border-2 rounded-lg m-auto py-3 bg-[#CDCDCD] justify-around flex'>
+                        <input type="text" placeholder='Ciudad' className='rounded-lg p-2 text-base h-12 w-[450px]' />
+                        <input type="date" placeholder='Fecha' className=' rounded-lg p-2 text-base h-12 w-80' />
+                        <input type="button" value="Buscar" className='bg-color-button hover:bg-color-button-hover color w-40 h-12 rounded-lg text-color-text-white font-bold text-base transition-all ease-in-out cursor-pointer' />
+                    </div>
+                </div>
+            </section>
+            <section className='bg-color-button'>
+                Services
+            </section>
+            <section className='bg-red-800'>
+                Clients
+            </section>
+            <section className='bg-blue-500'>
+                Preguntas Frecuentes
+            </section>
+            <section className='bg-purple-600'>
+                Search bar
+            </section>
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                banner: "url('https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3Vwd2s2MjE3MTU3Ni13aWtpbWVkaWEtaW1hZ2Uta293Z3dtNGYuanBn.jpg')",
+            },
+            colors: {
+                "color-bg": "#09595A",
+                "color-button": "#576702",
+                "color-button-hover": "#3F4A05",
+                "color-text-black": "#191414",
+                "color-text-white": "#FFFFFF",
+                "color-form-bg": "rgba(87, 103, 2, 0.35)",
+            },
+            fontFamily: {
+                inriasans: ["Inria Sans", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+    safelist: [
+        "color-bg",
+        "color-button",
+        "color-button-hover",
+        "color-text-black",
+        "color-text-white",
+        "color-form-bg",
+    ],
+};
+export default config;
