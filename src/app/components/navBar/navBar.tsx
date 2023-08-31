5ca8725030dc985a1b7d07595e8323ee4f6b3db8
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-08-31 14:28:22 -0500

    [FIX] navbar design

--- src/app/components/navBar/navBar.tsx
+++ src/app/components/navBar/navBar.tsx
@@ -1,37 +1,47 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-interface NavBarProps { }  // Define a props interface, even if it's empty for now
+interface NavBarProps {} // Define a props interface, even if it's empty for now
 
 const NavBar: React.FC<NavBarProps> = (props) => {
     return (
-        <div className="flex justify-around items-center h-16">
+        <div className="flex justify-around items-center h-16 bg-black bg-opacity-25 font-inriasans">
             <div>
-                <h1>LOGO</h1>
+                <Image
+                    src="/assets/logo2.png"
+                    height={39}
+                    width={98}
+                    alt="GolBooking logo"
+                ></Image>
             </div>
             <div className="flex items-center gap-8">
-                <div className="inline-flex items-start gap-12">
+                <div className="inline-flex items-start gap-12 text-black text-lg font-bold">
                     <Link href={"./"}>
-                        <button className="text-black font-medium text-base font-inria">Inicio</button>
+                        <button className="uppercase">Inicio</button>
                     </Link>
                     <Link href="/about-us">
-                        <button className="text-black font-medium text-base font-inria">Nosotros</button>
+                        <button className="uppercase">Nosotros</button>
                     </Link>
                     <Link href="/contact">
-                        <button className="text-black font-medium text-base font-inria">Contacto</button>
+                        <button className="uppercase">Contacto</button>
                     </Link>
                 </div>
                 <div className="flex gap-2">
                     <Link href="/log-in">
-                        <button className="text-white font-inria text-base font-medium bg-opacity-75 bg-[#576702] rounded px-8 py-1 border border-opacity-0 border-[#576702] hover:bg-[#3f4a05] transition-colors duration-300 border:transparent" >Ingreso</button>
+                        <button className="text-white bg-opacity-75 bg-color-button rounded px-8 py-1 border border-opacity-0 border-color-button hover:bg-color-button-hover transition-colors duration-300 border:transparent uppercase text-lg">
+                            Ingreso
+                        </button>
                     </Link>
                     <Link href="/log-in">
-                        <button className="text-[#576702] bg-transparent border border-opacity-75 border-[#576702] font-inria text-base font-medium rounded px-8 py-1 hover:bg-[#576702] hover:text-white transition-colors duration-300">Registro</button>
+                        <button className="text-color-button bg-white border border-opacity-75  rounded px-8 py-1 border-color-button hover:bg-color-button-hover hover:text-white transition-colors duration-300 uppercase text-lg">
+                            Registro
+                        </button>
                     </Link>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default NavBar;
