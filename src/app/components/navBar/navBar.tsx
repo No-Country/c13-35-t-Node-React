8d595bb15f3bbfb5546ebfe749009776283756fa
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-09-04 15:28:57 -0500

    [FIX] navbar styles

--- src/app/components/navBar/navBar.tsx
+++ src/app/components/navBar/navBar.tsx
@@ -6,30 +6,40 @@ interface NavBarProps {} // Define a props interface, even if it's empty for now
 
 const NavBar: React.FC<NavBarProps> = (props) => {
     return (
-        <div className="flex justify-around items-center h-16 bg-black bg-opacity-25 font-inriasans">
+        <div className="flex justify-around items-center h-16 bg-transparent bg-opacity-25 font-inriasans">
             <div>
-                <Image
-                    src="/assets/logo2.png"
-                    height={39}
-                    width={98}
-                    alt="GolBooking logo"
-                ></Image>
+                <Link href={"./"}>
+                    <Image
+                        src="/assets/logo2.png"
+                        height={39}
+                        width={98}
+                        alt="GolBooking logo"
+                    ></Image>
+                </Link>
             </div>
             <div className="flex items-center gap-8">
                 <div className="inline-flex items-start gap-12 text-black text-lg font-bold">
                     <Link href={"./"}>
-                        <button className="uppercase">Inicio</button>
+                        <button className="uppercase hover:underline hover:text-color-button transition-colors duration-300 hover:underline-offset-8">
+                            Inicio
+                        </button>
                     </Link>
                     <Link href="/about-us">
-                        <button className="uppercase">Nosotros</button>
+                        <button className="uppercase hover:underline hover:text-color-button transition-colors duration-300 hover:underline-offset-8">
+                            Nosotros
+                        </button>
                     </Link>
                     <Link href="/contact">
-                        <button className="uppercase">Contacto</button>
+                        <button className="uppercase hover:underline hover:text-color-button transition-colors duration-300 hover:underline-offset-8">
+                            Contacto
+                        </button>
                     </Link>
                 </div>
                 <div className="flex gap-2">
                     <Link href="/log-in">
-                        <button className="text-white font-inria text-base font-medium bg-opacity-75 bg-[#576702] rounded px-8 py-1 border border-opacity-0 border-[#576702] hover:bg-[#3f4a05] transition-colors duration-300 border:transparent" >Ingreso</button>
+                        <button className="text-white text-xl bg-opacity-75 bg-color-button rounded px-8 py-1 border border-opacity-0 border-color-button hover:bg-color-button-hover  border:transparent uppercase">
+                            Ingreso
+                        </button>
                     </Link>
                     <Link href="/register">
                         <button className="text-color-button bg-white border border-opacity-75  rounded px-8 py-1 border-color-button hover:bg-color-button-hover hover:text-white transition-colors duration-300 uppercase text-lg">
