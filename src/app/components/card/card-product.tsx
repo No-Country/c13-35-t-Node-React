5115aa91b0e420e9b05b276d8857d4965bcce6f2
Sergio Beleño <sergiosabh05@gmail.com>
2023-09-08 16:18:46 -0500

    add rating component

--- src/app/components/card/card-product.tsx
+++ src/app/components/card/card-product.tsx
@@ -1,16 +1,20 @@
-export default async function CardResult({
+import Rating from "@mui/material/Rating";
+
+export default function CardResult({
   id,
   nombre,
   descripcion,
   precio,
+  valoracion,
 }: {
   id: number;
   nombre: string;
   descripcion: string;
   precio: number;
+  valoracion: number;
 }) {
   return (
-    <article className="rounded-xl flex flex-col border-2 border-color-button">
+    <article className="rounded-xl flex flex-col border-2 justify-between border-color-button">
       <div className="p-5 rounded-2xl">
         <img
           src="https://images.unsplash.com/photo-1589487391730-58f20eb2c308?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80"
@@ -18,17 +22,16 @@ export default async function CardResult({
           className="w-full rounded-xl border-2 border-black w-[225px] h-[175px]"
         />
       </div>
-      <div className="flex flex-col justify-start px-5 py-2">
-        <h3 className="font-bold">{nombre}</h3>
-        <p>{descripcion}</p>
+      <div className="flex flex-col justify-start px-5 py-5">
+        <h3 className="font-bold text-base">{nombre}</h3>
+        <p className="text-left text-sm">{descripcion}</p>
         <a>ver mas...</a>
         <div className="flex items-center">
           <h3 className="font-bold">$ {precio}</h3>
           <p className="text-xs">(por hora)</p>
         </div>
-        <div>
-          <p>⚽⚽⚽⚽⚽</p>
-        </div>
+
+        <Rating value={valoracion} precision={0.1} readOnly />
       </div>
       <div className="w-full rounded-br-lg rounded-bl-lg bg-[#576702] flex justify-center items-center">
         <a
