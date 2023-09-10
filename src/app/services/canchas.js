fa313b6a7116b17213518097d561991b85b14c99
Sergio Beleño <sergiosabh05@gmail.com>
2023-09-10 17:31:44 -0500

    try catch function getCanchas

--- src/app/services/canchas.js
+++ src/app/services/canchas.js
@@ -1,9 +1,16 @@
-const API_URL = "https://goolbooking-api.onrender.com/api";
-export async function getCanchas() {
-  const response = await fetch(`${API_URL}/fields?limit=1`);
-  if (!response.ok) {
-    throw new Error("Error retrieving fields");
+export async function getCanchas(deporte, ciudad) {
+  try {
+    const response = await fetch(
+      `https://goolbooking-api.onrender.com/api/fields?deporte=${deporte}&ciudad=${ciudad}`
+    );
+
+    if (!response.ok) {
+      throw new Error("La ciudad no fue encontrada");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new error;
   }
-  const data = await response.json();
-  return data;
 }
