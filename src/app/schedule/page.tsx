161bdbf7f0291417f0b35846cb0ad048c050fe8b
TatianaSanchez01 <107728796+TatianaSanchez01@users.noreply.github.com>
2023-09-15 16:50:46 -0500

    [FIX] schedule style

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,64 +1,89 @@
-"use client";"use client";
+"use client";
+"use client";
 // pages/Schedule.js// pages/Schedule.js
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Schedule() {
-  const [selectedTime, setSelectedTime] = useState<string | null>(null);
+    const [selectedTime, setSelectedTime] = useState<string | null>(null);
 
-  // Datos estáticos
-  const date: string = '17 de agosto';
-  const times: string[] = [
-    '10:00 - 11:00', '11:00 - 12:00', '12:00 - 13:00',
-    '13:00 - 14:00', '14:00 - 15:00', '15:00 - 16:00',
-    '16:00 - 17:00', '17:00 - 18:00', '18:00 - 19:00',
-    '19:00 - 20:00', '20:00 - 21:00', '21:00 - 22:00'
-  ];
-  const unavailable: string[] = ['14:00 - 15:00', '20:00 - 21:00'];
+    // Datos estáticos
+    const date: string = "17 de agosto";
+    const times: string[] = [
+        "10:00 - 11:00",
+        "11:00 - 12:00",
+        "12:00 - 13:00",
+        "13:00 - 14:00",
+        "14:00 - 15:00",
+        "15:00 - 16:00",
+        "16:00 - 17:00",
+        "17:00 - 18:00",
+        "18:00 - 19:00",
+        "19:00 - 20:00",
+        "20:00 - 21:00",
+        "21:00 - 22:00",
+    ];
+    const unavailable: string[] = ["14:00 - 15:00", "20:00 - 21:00"];
 
-  return (
-    <div className="flex justify-center items-center min-h-screen relative">
-      <div className="flex flex-col items-center py-5 px-10 bg-f9f9f9 w-2/5 rounded-lg">
-        <h1 className="text-black text-4xl uppercase mb-4">TURNOS DISPONIBLES</h1>
-        <h2 className="text-black text-2xl mb-2">{date}</h2>
-        <h3 className="text-black text-xl mb-4">Valor $500/hora</h3>
+    return (
+        <div className="flex justify-center items-center min-h-screen relative">
+            <div className="flex flex-col items-center py-5 px-10 bg-white w-2/5 rounded-lg">
+                <h1 className="text-black text-4xl uppercase mb-4">
+                    TURNOS DISPONIBLES
+                </h1>
+                <h2 className="text-black text-2xl mb-2">{date}</h2>
+                <h3 className="text-black text-xl mb-4">Valor $500/hora</h3>
 
-        <div className="grid grid-cols-3 gap-4 mb-4 w-full">
-        {times.map((time: string, index: number) => (
-            <button
-              key={index}
-              className={`relative border rounded-lg p-4 
-                          ${unavailable.includes(time) ? 'bg-cdcdcd text-b6b4b4 cursor-not-allowed' : 'bg-f3f5f7 text-black hover:bg-gray-300'}
-                          ${selectedTime === time ? 'border-4 border-green-500' : 'border-black'}`}
-              disabled={unavailable.includes(time)}
-              onClick={() => setSelectedTime(time)}
-            >
-              {time}
-              {selectedTime === time && <div className="absolute top-0 right-0 w-5 h-5 bg-black rounded-full"></div>}
-            </button>
-          ))}
+                <div className="grid grid-cols-3 gap-4 mb-4 w-full">
+                    {times.map((time: string, index: number) => (
+                        <button
+                            key={index}
+                            className={`relative border rounded-lg p-4 
+                          ${
+                              unavailable.includes(time)
+                                  ? "bg-white text-color-button cursor-not-allowed"
+                                  : "bg-color-button text-white hover:text-black hover:bg-gray-300"
+                          }
+                          ${
+                              selectedTime === time
+                                  ? "border-4 border-green-500"
+                                  : "border-black"
+                          }`}
+                            disabled={unavailable.includes(time)}
+                            onClick={() => setSelectedTime(time)}
+                        >
+                            {time}
+                            {selectedTime === time && (
+                                <div className=""></div>
+                            )}
+                        </button>
+                    ))}
+                </div>
+                
+                <div className="w-full mb-4">
+                    <Link href="/search">
+                        <button className="bg-white border-2 border-color-button text-color-button py-2 rounded-lg w-full">
+                            VOLVER
+                        </button>
+                    </Link>
+                </div>
+                <div className="w-full mb-4">
+                    <Link href="/pago">
+                        <button className="bg-color-button text-color-text-white py-2 rounded-lg w-full hover:bg-color-button-hover ">
+                            CONFIRMAR RESERVA
+                        </button>
+                    </Link>
+                </div>
+                
+            </div>
+
+            <div
+                className="bg-cover bg-center w-full h-full absolute top-0 left-0"
+                style={{
+                    backgroundImage: "url(../../assets/horarios-fondo.jpg)",
+                    zIndex: "-1",
+                }}
+            ></div>
         </div>
-
-        <div className="w-full mb-4">
-    <Link href="/pago">
-        <button className="bg-576702 text-f9f9f9 py-2 rounded-lg w-full">CONFIRMAR LA RESERVA</button>
-    </Link>
-</div>
-<div className="w-full">
-    <Link href="/search">
-        <button className="bg-f9f9f9 border-2 border-576702 text-576702 py-2 rounded-lg w-full">VOLVER</button>
-    </Link>
-</div>
-
-      </div>
-      
-      <div
-        className="bg-cover bg-center w-full h-full absolute top-0 left-0"
-        style={{
-          backgroundImage: 'url(../../assets/horarios-fondo.jpg)',
-          zIndex: '-1'
-        }}
-      ></div>
-    </div>
-  );
+    );
 }
