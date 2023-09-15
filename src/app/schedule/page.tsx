8d9be259ddf92c1b11a891d006aba8e3e65a4dd4
Marcos A <aravenap@gmail.com>
2023-09-15 17:58:15 -0300

    modified schedule tsx code

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -4,17 +4,17 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Schedule() {
-  const [selectedTime, setSelectedTime] = useState(null);
+  const [selectedTime, setSelectedTime] = useState<string | null>(null);
 
   // Datos estáticos
-  const date = '17 de agosto';
-  const times = [
+  const date: string = '17 de agosto';
+  const times: string[] = [
     '10:00 - 11:00', '11:00 - 12:00', '12:00 - 13:00',
     '13:00 - 14:00', '14:00 - 15:00', '15:00 - 16:00',
     '16:00 - 17:00', '17:00 - 18:00', '18:00 - 19:00',
     '19:00 - 20:00', '20:00 - 21:00', '21:00 - 22:00'
   ];
-  const unavailable = ['14:00 - 15:00', '20:00 - 21:00'];
+  const unavailable: string[] = ['14:00 - 15:00', '20:00 - 21:00'];
 
   return (
     <div className="flex justify-center items-center min-h-screen relative">
@@ -24,7 +24,7 @@ export default function Schedule() {
         <h3 className="text-black text-xl mb-4">Valor $500/hora</h3>
 
         <div className="grid grid-cols-3 gap-4 mb-4 w-full">
-          {times.map((time, index) => (
+        {times.map((time: string, index: number) => (
             <button
               key={index}
               className={`relative border rounded-lg p-4 
